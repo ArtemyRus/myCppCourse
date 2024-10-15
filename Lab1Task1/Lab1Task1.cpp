8eff0aba34ac6f377605eeae066aa6d879402b51
Artemy <Artemy@DESKTOP-HR22CC5>
2024-10-16 00:31:50 +0500

    Добавьте файлы проекта.

---
+++ Lab1Task1/Lab1Task1.cpp
@@ -0,0 +1,31 @@
+﻿//Ознакомиться со структурой программы на языке C++. Для этого
+//выполнить программу возведения числа 2 в степень. Модифицировать
+//программу так, чтобы вычислялась степень с любым основанием.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, k, value;
+    int power(int, int);
+    int getInputNumber();
+    value = getInputNumber();
+    for (i = 0; i < 10; i++) {
+        k = power(value, i);
+        cout << "i = " << i << ", k = " << k << endl;
+    }
+}
+
+int power(int x, int n) {
+    int i, p = 1;
+    for (i = 1; i <= n; i++)
+        p *= x;
+    return p;
+}
+
+int getInputNumber() {
+    int value;
+    cout << "Enter power base: ";
+    cin >> value;
+    return value;
+}
