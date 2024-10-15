2739570646cff7f8ea350e658e739b857987a169
Artemy <Artemy@DESKTOP-HR22CC5>
2024-10-16 02:12:10 +0500

    task3

---
+++ Lab1Task3/Lab1Task3.cpp
@@ -0,0 +1,32 @@
+﻿//Написать программу, которая меняет местами значения двух
+//переменных. В качестве параметра использовать int, int* и int&.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 2, b = 5;
+	void obmen1(int, int); // для записи значения в переменную необходимы ссылки в аргументах функции
+	void obmen2(int*, int*);
+	void obmen3(int&, int&);
+	cout << "Before swap: a = " << a << ", b = " << b << endl;
+	obmen2(&a, &b);
+	cout << "After swap: a = " << a << ", b = " << b << endl;
+	cout << "Before swap: a = " << a << ", b = " << b << endl;
+	obmen3(a, b);
+	cout << "After swap: a = " << a << ", b = " << b << endl;
+
+}
+
+void obmen2(int* a, int* b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void obmen3(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
