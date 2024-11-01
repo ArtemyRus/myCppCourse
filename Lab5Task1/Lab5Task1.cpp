1e7508f7bb4e5636de2c3d2ac038853116b73c41
Artemy <Artemy@DESKTOP-HR22CC5>
2024-11-01 21:07:13 +0500

    lab5

---
+++ Lab5Task1/Lab5Task1.cpp
@@ -0,0 +1,80 @@
+﻿// Программа, выводящая содержимое текстового файла на экран.
+// Дополнить программу функцией подсчета "пустых" и "непустых" символов в файле.
+// Дополнить программу функцией поиска в файле заданной подстроки.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char ch[50];
+	string fileName = "myFile.txt";
+    void printText(ifstream&, string);
+    void countSymbols(ifstream&, string);
+    void findSubStr(ifstream&, string, string);
+
+	ifstream in;
+    printText(in, fileName);
+    countSymbols(in, fileName);
+    string subStr = "hell";
+    findSubStr(in, subStr, fileName);
+	in.close();
+}
+
+void printText(ifstream& in, string fileName) {
+    in.open(fileName);
+    if (in.is_open())
+    {
+        string s;
+        while (!in.eof())
+        {
+            getline(in, s);
+            cout << s << endl;
+        }
+        in.close();
+    }
+    else cout << "Ошибка чтения файла" << endl;
+}
+
+void countSymbols(ifstream& in, string fileName) {
+    in.open(fileName);
+    if (in.is_open())
+    {
+        char ch;
+        int blank = 0, symb = 0;
+        while (in.get(ch))
+        {
+            if (isspace(ch)) blank++;
+            else symb++;
+        }
+        cout << "Количество пустых символов в файле: " << blank << endl;
+        cout << "Количество непустых символов в файле: " << symb << endl;
+        in.close();
+    }
+    else cout << "Ошибка чтения файла" << endl;
+}
+
+void findSubStr(ifstream& in, string str, string fileName) {
+    in.open(fileName);
+    if (in.is_open())
+    {
+        int countLine = 1, i;
+        string s;
+        while (!in.eof())
+        {
+            getline(in, s);
+            if ((i = s.find(str)) != string::npos) {
+                cout << "Строка найдена в тексте: " << countLine << " строка, " << i << " символ." << endl;
+                in.close();
+                return;
+            }
+            countLine++;
+        }
+        cout << "Строка не найдена" << endl;
+        in.close();
+    }
+    else cout << "Ошибка чтения файла" << endl;
+}
---
+++ Lab5Task2/Lab5Task2.cpp
@@ -0,0 +1,59 @@
+﻿// Написать программу-шифратор файлов.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string key;
+	cout << "Введите ключ: " << endl;
+	cin >> key;
+	ifstream in("text.txt");
+	ofstream out("encryptedText.txt");
+	void printText(ifstream&, string);
+	if (in.is_open() && out.is_open()) {
+		char ch;
+		int i = 0;
+		while (in.get(ch)) {
+			out << (char)(ch ^ key[i]);
+		}
+		in.close();
+		out.close();
+	}
+	else cout << "Ошибка чтения / записи файла." << endl;
+
+	in.open("encryptedText.txt");
+	out.open("decryptedText.txt");
+	if (in.is_open() && out.is_open()) {
+		char ch;
+		int i = 0;
+		while (in.get(ch)) {
+			out << (char)(ch ^ key[i]);
+		}
+		in.close();
+		out.close();
+	}
+	else cout << "Ошибка чтения / записи файла." << endl;
+
+	printText(in, "text.txt");
+	printText(in, "encryptedText.txt");
+	printText(in, "decryptedText.txt");
+}
+
+void printText(ifstream& in, string fileName) {
+	in.open(fileName);
+	if (in.is_open())
+	{
+		string s;
+		while (!in.eof())
+		{
+			getline(in, s);
+			cout << s << endl;
+		}
+		in.close();
+	}
+	else cout << "Ошибка чтения файла" << endl;
+}
